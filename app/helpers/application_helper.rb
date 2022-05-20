30eb3d61467ace3f000f55fb26dd2db6fd681c25
delicha <shoheiyamamoto919@gmail.com>
2022-05-20 22:40:20 +0900

    refactoring impression_comment

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -23,96 +23,77 @@ module ApplicationHelper
     sum_impression
   end
 
-  # def impression_comment(sex)
-  #   ary = [
-  #     ['かわいいが渋滞してる！','ヤバすぎ！！！','文句なし！！','ストライク！','やるな。','まぁまぁかな。','イマイチかな。','自分が見えてないと思う。','まずは自分磨きをしよう。','修行編エピソード１的な。'],
-  #     ['カッコいいが渋滞してる！','ドキドキする！！！','イケてる！！','ストライク！','タイプ！','やるわね。','少し実力不足。','まだ自分が見えてない。','まずは自分磨きね。','あきめないで。']
-  #   ]
-  #   case @sum
-  #   when @sum>= 90 then
-  #     ary[@user.sex][0]
-  #   when @sum>= 80 then
-  #     ary[@user.sex][1]
-  #   when @sum>= 70 then
-  #     ary[@user.sex][2]
-  #   when @sum>= 60 then
-  #     ary[@user.sex][3]
-  #   when @sum>= 50 then
-  #     ary[@user.sex][4]
-  #   when @sum>= 40 then
-  #     ary[@user.sex][5]
-  #   when @sum>= 30 then
-  #     ary[@user.sex][6]
-  #   when @sum>= 20 then
-  #     ary[@user.sex][7]
-  #   when @sum>= 10 then
-  #     ary[:@ser.sex][8]
-  #   when @sum>= 0 then
-  #     ary[@user.sex][9]
+  def impression_comment(sex)
+    num = impression_total / 10
+    num = num.floor
+    ary = [
+      ['修行編エピソード１的な。','まずは自分磨きをしよう。', '自分が見えてないと思う。', 'イマイチかな。', 'まぁまぁかな。', 'やるな。', 'ストライク！', '文句なし！！', 'ヤバすぎ！！！', 'かわいいが渋滞してる！'],
+      ['あきめないで。', 'まずは自分磨きね。', 'まだ自分が見えてない。', '少し実力不足。', 'やるわね。', 'タイプ！', 'ストライク！', 'イケてる！！', 'ドキドキする！！！', 'カッコいいが渋滞してる！']
+    ]
+    ary[sex][num]
+  end
+
+  # def impression_comment
+  #   if @sum >= 90
+  #     if @user.sex == 0
+  #       'かわいいが渋滞してる！'
+  #     else
+  #       'カッコいいが渋滞してる！'
+  #     end
+  #   elsif @sum >= 80
+  #     if @user.sex == 0
+  #       'ヤバすぎ！！！'
+  #     else
+  #       'ドキドキする！！！'
+  #     end
+  #   elsif @sum >= 70
+  #     if @user.sex == 0
+  #       '文句なし！！'
+  #     else
+  #       'イケてる！！'
+  #     end
+  #   elsif @sum >= 60
+  #     if @user.sex == 0
+  #       'ストライク！'
+  #     else
+  #       'タイプ！'
+  #     end
+  #   elsif @sum >= 50
+  #     if @user.sex == 0
+  #       'やるな。'
+  #     else
+  #       'やるわね。'
+  #     end
+  #   elsif @sum >= 40
+  #     if @user.sex == 0
+  #       'まぁまぁかな。'
+  #     else
+  #       'まぁまぁ。'
+  #     end
+  #   elsif @sum >= 30
+  #     if @user.sex == 0
+  #       'イマイチかな。'
+  #     else
+  #       '少し実力不足。'
+  #     end
+  #   elsif @sum >= 20
+  #     if @user.sex == 0
+  #       '自分が見えてないと思う。'
+  #     else
+  #       'まだ自分が見えてない。'
+  #     end
+  #   elsif @sum >= 10
+  #     if @user.sex == 0
+  #       'まずは自分磨きをしよう。'
+  #     else
+  #       'まずは自分磨きね。'
+  #     end
+  #   elsif @user.sex == 0
+  #     '修行編エピソード１的な。'
+  #   else
+  #     'あきめないで。'
   #   end
   # end
-
-  def impression_comment
-    if @sum >= 90
-      if @user.sex == 0
-        'かわいいが渋滞してる！'
-      else
-        'カッコいいが渋滞してる！'
-      end
-    elsif @sum >= 80
-      if @user.sex == 0
-        'ヤバすぎ！！！'
-      else
-        'ドキドキする！！！'
-      end
-    elsif @sum >= 70
-      if @user.sex == 0
-        '文句なし！！'
-      else
-        'イケてる！！'
-      end
-    elsif @sum >= 60
-      if @user.sex == 0
-        'ストライク！'
-      else
-        'タイプ！'
-      end
-    elsif @sum >= 50
-      if @user.sex == 0
-        'やるな。'
-      else
-        'やるわね。'
-      end
-    elsif @sum >= 40
-      if @user.sex == 0
-        'まぁまぁかな。'
-      else
-        'まぁまぁ。'
-      end
-    elsif @sum >= 30
-      if @user.sex == 0
-        'イマイチかな。'
-      else
-        '少し実力不足。'
-      end
-    elsif @sum >= 20
-      if @user.sex == 0
-        '自分が見えてないと思う。'
-      else
-        'まだ自分が見えてない。'
-      end
-    elsif @sum >= 10
-      if @user.sex == 0
-        'まずは自分磨きをしよう。'
-      else
-        'まずは自分磨きね。'
-      end
-    elsif @user.sex == 0
-      '修行編エピソード１的な。'
-    else
-      'あきめないで。'
-    end
-  end
 
   def user_demo_image
     if current_user.sex == 0
