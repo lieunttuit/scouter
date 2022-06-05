60e1f61a61bc674297d538866cc96d9adb5de954
delicha <shoheiyamamoto919@gmail.com>
2022-06-05 19:52:13 +0900

    heart test13

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -22,8 +22,8 @@ module ApplicationHelper
     @evaluations = Evaluation.where(evaluatee_id: @user.id).includes(:user).where(users: { generation: num })
     @sum = @evaluations.sum { |hash| hash[:evaluation_point] }
     point = @sum / @evaluations.count
-    point = @point.round
-    for i in 1..point do
+    point = point.round
+    for i in 0..point do
       if i == 0
         ''
       else
