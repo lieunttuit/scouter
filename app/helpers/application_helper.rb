5f357755beb2a7c2990fd89d0e905f1ccb93c633
delicha <shoheiyamamoto919@gmail.com>
2022-06-05 20:53:27 +0900

    heart test26

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,11 +16,10 @@ module ApplicationHelper
   def impression_each_generation(num)
     @evaluations = Evaluation.where(evaluatee_id: @user.id).includes(:user).where(users: { generation: num })
     @sum = @evaluations.sum { |hash| hash[:evaluation_point] }
-    @point = @sum * 10 / @evaluations.count.to_i
-    if @point == 0 
+    if @sum == 0 
       'まだ評価はありません' 
     elsif 
-      @sum * 10 / @evaluations.count
+      @sum / @evaluations.count
     end
     # if @sum == 0 
     #   'まだ評価はありません'
