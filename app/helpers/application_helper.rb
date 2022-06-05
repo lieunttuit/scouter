706fd1373dab1186b7f0aa1a3445241248ccfae7
delicha <shoheiyamamoto919@gmail.com>
2022-06-05 19:54:21 +0900

    heart test14

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -21,15 +21,15 @@ module ApplicationHelper
   def heart_each_generation(num)
     @evaluations = Evaluation.where(evaluatee_id: @user.id).includes(:user).where(users: { generation: num })
     @sum = @evaluations.sum { |hash| hash[:evaluation_point] }
-    point = @sum / @evaluations.count
-    point = point.round
-    for i in 0..point do
-      if i == 0
-        ''
-      else
-        '❤️'
-      end
-    end
+    @point = @sum / @evaluations.count
+    @point.round
+    # for i in 0..point do
+    #   if i == 0
+    #     ''
+    #   else
+    #     '❤️'
+    #   end
+    # end
   end
 
   def impression_total
