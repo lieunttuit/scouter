eb710eec6ec99feac9400600d769440e1d5437aa
delicha <shoheiyamamoto919@gmail.com>
2022-06-06 16:52:12 +0900

    fix impression_each_generation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,12 +16,7 @@ module ApplicationHelper
   def impression_each_generation(num)
     @evaluations = Evaluation.where(evaluatee_id: @user.id).includes(:user).where(users: { generation: num })
     @sum = @evaluations.sum { |hash| hash[:evaluation_point] }
-    @point = @sum * 10 / @evaluations.count
-    if @point == 0
-      '❤️'
-    else
-      'まだ評価はありません'
-    end
+    @sum == 0 ? 'まだ評価はありません' : @sum * 10 / @evaluations.count
   end
 
   def impression_total
