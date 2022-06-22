0b61408804476ed45117bad06e06f3224437bb5c
delicha <shoheiyamamoto919@gmail.com>
2022-06-22 12:10:23 +0900

    fix application-controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,30 @@
 class ApplicationController < ActionController::Base
-  helper_method :current_user
-  before_action :login_required
-
+  protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  # helper_method :after_sign_in_path_for
+  # helper_method :after_sign_out_path_for
   private
 
-  def current_user
-    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
-  end
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :sex, :hobby, :character, :generation, :point, :image, sub_images: []])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:name, :sex, :hobby, :character, :generation, :point, :image, sub_images: []])
+    end
 
-  def login_required
-    redirect_to login_path unless current_user
-  end
+    def after_sign_in_path_for(resource)
+      case resource
+      when User
+        users_path 
+      when Admin
+        admins_users_path
+      end
+    end
+    
+    def after_sign_out_path_for(resource)
+      case resource
+      when User
+        root_path 
+      when Admin
+        root_path
+      end
+    end
 end
