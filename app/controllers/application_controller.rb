5accc69d405fb312dc87c75c53f7bb38f0cb9386
delicha <shoheiyamamoto919@gmail.com>
2022-07-06 23:03:39 +0900

    deletehelper_method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
-  # helper_method :after_sign_in_path_for
-  # helper_method :after_sign_out_path_for
   private
 
     def configure_permitted_parameters
