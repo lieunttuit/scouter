867e77a8ff5ea27e83cd18b9a16f2540aa16f7d9
delicha <shoheiyamamoto919@gmail.com>
2022-07-09 15:59:16 +0900

    organize admin cont

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,30 +1,12 @@
 class Admin::UsersController < ApplicationController
   before_action :require_admin
-  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  # before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   def index
     @q = User.all.ransack(params[:q])
     @users = @q.result(distinct: true).order('updated_at DESC').paginate(page: params[:page], per_page: 6)
     @male = User.where(sex: 1)
     @female = User.where(sex: 0)
-  end
-
-  def show
-  end
-
-  def new
-  end
-
-  def create
-  end
-
-  def edit
-  end
-
-  def update
-  end
-
-  def destroy
   end
 
   def evaluation
@@ -37,12 +19,4 @@ class Admin::UsersController < ApplicationController
     redirect_to root_path unless current_user.admin?
   end
 
-  def user_params
-    params.require(:user).permit(:name, :email, :admin, :password, :password_confirmation, :sex, :character, :hobby,
-                                 :generation, :point, :image, sub_images: [])
-  end
-
-  def set_user
-    @user = User.find(params[:id])
-  end
 end
