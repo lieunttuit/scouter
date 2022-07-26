643228525c4db0adde5fe6a73bee0262c30bf626
delicha <shoheiyamamoto919@gmail.com>
2022-07-26 14:55:29 +0900

    redirect_user in home

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,12 @@
 class HomeController < ApplicationController
   skip_before_action :authenticate_user!
+  before_action :redirect_user
+  
   def index
+  end
+
+  private
+  def redirect_user
+    redirect_to users_path if current_user
   end
 end
